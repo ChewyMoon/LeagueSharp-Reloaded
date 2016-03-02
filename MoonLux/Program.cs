018bf293294ff078ae499eb8c4eaa93fc0a6ba0b
ChewyMoon <chewymoonleaguesharp@gmail.com>
2016-03-02 17:15:05 -0500

    MoonLux 1.0.0.3 Better Q Prediction

--- MoonLux/Program.cs
+++ MoonLux/Program.cs
@@ -608,12 +608,13 @@
                 return;
             }
 
-            Q = new Spell(SpellSlot.Q, 1200);
+            Q = new Spell(SpellSlot.Q, 1175);
             W = new Spell(SpellSlot.W, 1075);
             E = new Spell(SpellSlot.E, 1075);
             R = new Spell(SpellSlot.R, 3000);
 
-            Q.SetSkillshot(0.25f, 70f, 1200f, true, SkillshotType.SkillshotLine);
+            Q.SetSkillshot(0.25f, 50f, 1200f, true, SkillshotType.SkillshotLine);
+            Q.SetSkillshot(0.25f, 50f, 1200f, true, SkillshotType.SkillshotLine);
             W.SetSkillshot(0.25f, 110f, 1200f, false, SkillshotType.SkillshotLine);
             E.SetSkillshot(0.3f, 250f, 1050f, false, SkillshotType.SkillshotCircle);
             R.SetSkillshot(1f, 110f, float.MaxValue, false, SkillshotType.SkillshotLine);

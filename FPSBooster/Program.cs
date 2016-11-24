0810d6cee02a2d0162c502aa9cea6f654ebab27e
ChewyMoon <ChewyMoon@users.noreply.github.com>
2016-11-23 23:36:24 -0500

    Limit Drawing.OnDraw & OnEndScene, moved fps boosting code to a single method.

--- FPSBooster/Program.cs
+++ FPSBooster/Program.cs
@@ -10,43 +10,168 @@
 
     class Program
     {
-        #region Properties
+        #region Public Properties
 
         /// <summary>
-        /// Gets or sets a value indicating whether {CC2D43FA-BBC4-448A-9D0B-7B57ADF2655C}[first update].
+        ///     Gets or sets a value indicating whether this is the first game update.
         /// </summary>
         /// <value>
-        /// {D255958A-8513-4226-94B9-080D98F904A1}  <c>true</c> if [first update]; otherwise, <c>false</c>.
+        ///     <c>true</c> if this is the first game update; otherwise, <c>false</c>.
         /// </value>
-        private static bool FirstUpdate { get; set; } = true;
+        public static bool FirstGameUpdate { get; set; } = true;
 
         /// <summary>
-        /// Gets or sets the last update time.
+        ///     Gets or sets the last end scene time.
         /// </summary>
         /// <value>
-        /// The last update time.
+        ///     The last end scene time.
         /// </value>
-        private static float LastUpdateTime { get; set; } = Environment.TickCount;
+        public static float LastEndSceneTime { get; set; } = Environment.TickCount;
 
         /// <summary>
-        /// Gets or sets the on update delegates.
+        ///     Gets or sets the last game draw time.
         /// </summary>
         /// <value>
-        /// The on update delegates.
+        ///     The last game draw time.
         /// </value>
-        private static List<Delegate> OnUpdateDelegates { get; set; }
+        public static float LastGameDrawTime { get; set; } = Environment.TickCount;
 
         /// <summary>
-        /// Gets the update time.
+        ///     Gets or sets the last update time.
         /// </summary>
         /// <value>
-        /// The update time.
+        ///     The last update time.
         /// </value>
-        private static float UpdateTime => (1 / 30f) * 1000;
+        public static float LastGameUpdateTime { get; set; } = Environment.TickCount;
+
+        /// <summary>
+        ///     Gets or sets the on draw delegates.
+        /// </summary>
+        /// <value>
+        ///     The on draw delegates.
+        /// </value>
+        public static List<Delegate> OnDrawDelegates { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the on end scene delegates.
+        /// </summary>
+        /// <value>
+        ///     The on end scene delegates.
+        /// </value>
+        public static List<Delegate> OnEndSceneDelegates { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the on update delegates.
+        /// </summary>
+        /// <value>
+        ///     The on update delegates.
+        /// </value>
+        public static List<Delegate> OnGameUpdateDelegates { get; set; }
+
+        /// <summary>
+        ///     Gets the update time.
+        /// </summary>
+        /// <value>
+        ///     The update time.
+        /// </value>
+        public static float UpdateTime => (1 / 30f) * 1000;
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        /// <summary>
+        ///     Updates the event.
+        /// </summary>
+        /// <param name="delegates">The delegates.</param>
+        /// <param name="eventClass">The event class.</param>
+        /// <param name="fieldName">Name of the field.</param>
+        /// <param name="localUpdateMethodName">Name of the local update method.</param>
+        /// <param name="lastUpdateTime">The last update time.</param>
+        /// <param name="updateTime">The update time.</param>
+        /// <returns></returns>
+        public static List<Delegate> UpdateEvent(
+            List<Delegate> delegates,
+            Type eventClass,
+            string fieldName,
+            string localUpdateMethodName,
+            float lastUpdateTime,
+            float updateTime)
+        {
+            if (delegates == null)
+            {
+                var memberInfo = eventClass.GetField(fieldName, BindingFlags.Static | BindingFlags.NonPublic);
+                if (memberInfo == null)
+                {
+                    Console.WriteLine("Error trying to obtain MemberInfo from the {0} class.", eventClass.FullName);
+                    return null;
+                }
+
+                var updateHandlers = (List<Delegate>)memberInfo.GetValue(null);
+                if (updateHandlers == null)
+                {
+                    Console.WriteLine("Error trying to obain UpdateHandlers from the {0} class.", eventClass.FullName);
+                    return null;
+                }
+
+                delegates = new List<Delegate>();
+                delegates.AddRange(
+                    updateHandlers.Where(
+                        x =>
+                            x.Method.Name != localUpdateMethodName
+                            && x.Method.DeclaringType?.FullName != "LeagueSharp.Sandbox.LeagueSharpBootstrapper"));
+
+                delegates.RemoveAll(x => x.Method.Name != localUpdateMethodName);
+
+                return delegates;
+            }
+
+            if (Environment.TickCount - lastUpdateTime < updateTime)
+            {
+                return delegates;
+            }
+
+            delegates.ForEach(x => x.Method.Invoke(x.Target, new object[] { EventArgs.Empty }));
+            return delegates;
+        }
 
         #endregion
 
         #region Methods
+
+        /// <summary>
+        ///     The OnDraw event.
+        /// </summary>
+        /// <param name="args">The <see cref="EventArgs" /> instance containing the event data.</param>
+        private static void FpsBoosterDraw(EventArgs args)
+        {
+            OnDrawDelegates = UpdateEvent(
+                OnDrawDelegates,
+                typeof(Drawing),
+                "DrawHandlers",
+                nameof(FpsBoosterDraw),
+                LastGameDrawTime,
+                UpdateTime);
+
+            LastGameDrawTime = Environment.TickCount;
+        }
+
+        /// <summary>
+        ///     The OnEndScene event.
+        /// </summary>
+        /// <param name="args">The <see cref="EventArgs" /> instance containing the event data.</param>
+        private static void FpsBoosterEndScene(EventArgs args)
+        {
+            OnEndSceneDelegates = UpdateEvent(
+                OnEndSceneDelegates,
+                typeof(Drawing),
+                "EndSceneHandlers",
+                nameof(FpsBoosterEndScene),
+                LastEndSceneTime,
+                UpdateTime);
+
+            LastEndSceneTime = Environment.TickCount;
+        }
 
         /// <summary>
         ///     The OnGameUpdate event.
@@ -55,47 +180,21 @@
         private static void FpsBoosterUpdate(EventArgs args)
         {
             // Skip the first update to make sure all events are loaded in
-            if (FirstUpdate)
+            if (FirstGameUpdate)
             {
-                FirstUpdate = false;
+                FirstGameUpdate = false;
                 return;
             }
 
-            if (OnUpdateDelegates == null)
-            {
-                var memberInfo = typeof(Game).GetField("UpdateHandlers", BindingFlags.Static | BindingFlags.NonPublic);
-                if (memberInfo == null)
-                {
-                    Console.WriteLine("Error trying to obtain MemberInfo from the Game class.");
-                    return;
-                }
+            OnGameUpdateDelegates = UpdateEvent(
+                OnGameUpdateDelegates,
+                typeof(Game),
+                "UpdateHandlers",
+                nameof(FpsBoosterUpdate),
+                LastGameUpdateTime,
+                UpdateTime);
 
-                var updateHandlers = (List<GameUpdate>)memberInfo.GetValue(null);
-                if (updateHandlers == null)
-                {
-                    Console.WriteLine("Error trying to obain UpdateHandlers from the Game class.");
-                    return;
-                }
-
-                OnUpdateDelegates = new List<Delegate>();
-                OnUpdateDelegates.AddRange(
-                    updateHandlers.Where(
-                        x =>
-                            x.Method.Name != "FpsBoosterUpdate"
-                            && x.Method.DeclaringType?.FullName != "LeagueSharp.Sandbox.LeagueSharpBootstrapper"));
-
-                updateHandlers.RemoveAll(x => x.Method.Name != "FpsBoosterUpdate");
-
-                return;
-            }
-
-            if (Environment.TickCount - LastUpdateTime < UpdateTime)
-            {
-                return;
-            }
-
-            OnUpdateDelegates.ForEach(x => x.Method.Invoke(x.Target, new object[] { EventArgs.Empty }));
-            LastUpdateTime = Environment.TickCount;
+            LastGameUpdateTime = Environment.TickCount;
         }
 
         static void Main(string[] args)
@@ -104,12 +203,14 @@
         }
 
         /// <summary>
-        /// Raises the <see cref="E:GameLoad" /> event.
+        ///     Raises the <see cref="E:GameLoad" /> event.
         /// </summary>
-        /// <param name="args">The <see cref="EventArgs"/> instance containing the event data.</param>
+        /// <param name="args">The <see cref="EventArgs" /> instance containing the event data.</param>
         private static void OnGameLoad(EventArgs args)
         {
             Game.OnUpdate += FpsBoosterUpdate;
+            Drawing.OnDraw += FpsBoosterDraw;
+            Drawing.OnEndScene += FpsBoosterEndScene;
         }
 
         #endregion
